0daf6ab5314e604136eb5974d6cdbf365e3f7f47
Dn <patt.daniel@gmail.com>
2014-10-12 07:21:54 +0000

    Now accepting index.html in index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var buffer = fs.readFileSync(index.html);
+var bufString = buffer.toString();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+//  response.send('Hello World2!')
+  response.send(bufString);
 })
 
 app.listen(app.get('port'), function() {
