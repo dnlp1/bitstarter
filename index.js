5f7e57d255c566d582b6c34c7dce0516f4719e5a
Dn <patt.daniel@gmail.com>
2014-10-12 07:29:40 +0000

    Trying to fix bug in buffer logic 3rd attempt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
-var buffer = fs.readFileSync(index.html);
-var bufString = buffer.toString();
+//var buffer = fs.readFileSync(index.html);
+//var bufString = buffer.toString();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
